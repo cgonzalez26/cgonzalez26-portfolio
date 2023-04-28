bf0797cc80e2c528cc3aeda5a639cd3206e72ebe
cristian gonzalez <crisgonzalez26@gmail.com>
2023-04-28 12:12:54 -0300

    Fix datos

--- src/data/clientsData.js
+++ src/data/clientsData.js
@@ -1,54 +1,45 @@
 // Import images
-import AmazonImage from '../images/brands/amazon_gray.png';
-import SonyImage from '../images/brands/sony_gray.png';
-import AdidasImage from '../images/brands/adidas_gray.png';
-import FilaImage from '../images/brands/fila_gray.png';
-import NBImage from '../images/brands/nb_gray.png';
-import SamsungImage from '../images/brands/samsung_gray.png';
-import CanonImage from '../images/brands/canon_gray.png';
-import PumaImage from '../images/brands/puma_gray.png';
-
 export const clientsHeading = 'Some of the brands that trust me';
 
 export const clientsData = [
 	{
 		id: 1,
 		title: 'Amazon',
-		img: AmazonImage,
+		img: '',
 	},
 	{
 		id: 2,
 		title: 'Sony',
-		img: SonyImage,
+		img: '',
 	},
 	{
 		id: 3,
 		title: 'Adidas',
-		img: AdidasImage,
+		img: '',
 	},
 	{
 		id: 4,
 		title: 'FILA',
-		img: FilaImage,
+		img: '',
 	},
 	{
 		id: 5,
 		title: 'NB',
-		img: NBImage,
+		img: '',
 	},
 	{
 		id: 6,
 		title: 'SAMSUNG',
-		img: SamsungImage,
+		img: '',
 	},
 	{
 		id: 7,
 		title: 'CANON',
-		img: CanonImage,
+		img: '',
 	},
 	{
 		id: 8,
 		title: 'PUMA',
-		img: PumaImage,
+		img: '',
 	},
 ];
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,6 +1,5 @@
 import AboutMeBio from '../components/about/AboutMeBio';
-/*import AboutCounter from '../components/about/AboutCounter';
-import AboutClients from '../components/about/AboutClients';*/
+//import AboutCounter from '../components/about/AboutCounter';
 import { AboutMeProvider } from '../context/AboutMeContext';
 import { motion } from 'framer-motion';
 
@@ -24,15 +23,7 @@ const About = () => {
 			>
 				<AboutCounter />
 			</motion.div>			
-
-			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1, delay: 1 }}
-				exit={{ opacity: 0 }}
-				className="container mx-auto"
-			>
-				<AboutClients />
-			</motion.div>*/
+		*/
 			}
 		</AboutMeProvider>
 	);
